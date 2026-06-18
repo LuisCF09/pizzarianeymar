e7033ce26729a392c62364e94697d4be61771a24
LuisCF09 <rm99645@estudante.fieb.edu.br>
2026-06-18 15:29:11 +0000

    salvando progresso

---
+++ src/main/java/com/itb/inf2fm/pizzarianeymar/model/services/ProdutoServices.java
@@ -0,0 +1,10 @@
+package com.itb.inf2fm.pizzarianeymar.model.services;
+
+import java.util.List;
+
+public class ProdutoServices {
+
+    //Lista temporária de produtos
+
+    private static List<Produto> PRODUTOS = new ArrayList<>();
+}
